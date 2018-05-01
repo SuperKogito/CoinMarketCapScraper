2ec3195054869488d5f2f75aa6c0bdc61d3d4901
kogito <kogito@lenovo>
2018-05-02 01:31:16 +0200

    update code: transfer code to classes style

--- scrape.py
+++ scrape.py
@@ -10,37 +10,41 @@ from bs4 import BeautifulSoup
 import plotly.graph_objs as go
 
 
-url = input('Insert your URL example(https://coinmarketcap.com/currencies/bitcoin/historical-data/...):\n')  
-r = requests.get(url)
-soup = BeautifulSoup(r.text, 'lxml')
+class Scraper():
 
-data = []
-table = soup.find('table', id='')
-for row in table.find_all('tr'):
-    cells = row.findChildren('td')
-    values = []
-    for cell in cells:
-        value = cell.string
-        values.append(value)
-    try: 
-        Date = values[0]
-        Open = values[1]
-        High = values[2]
-        Low =  values[3]
-        Close = values[4]
-        Volume = values[5]
-        MarketCap = values[6]
-    except IndexError:
-        continue
-    data.append([Date, Open, High, Low, Close, Volume, MarketCap])
-
-for item in data:
-    print(item)      
-    
-f = open('ScrapedData.csv', 'w')
-with f:
-    writer = csv.writer(f)
-    writer.writerow(['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'MarketCap'])
-
-    for row in data:
-        writer.writerow(row)
+    def process(self, url):
+        r = requests.get(url)
+        soup = BeautifulSoup(r.text, 'lxml')
+        
+        data = []
+        table = soup.find('table', id='')
+        for row in table.find_all('tr'):
+            cells = row.findChildren('td')
+            values = []
+            for cell in cells:
+                value = cell.string
+                values.append(value)
+            try: 
+                Date = values[0]
+                Open = values[1]
+                High = values[2]
+                Low =  values[3]
+                Close = values[4]
+                Volume = values[5]
+                MarketCap = values[6]
+            except IndexError:
+                continue
+            data.append([Date, Open, High, Low, Close, Volume, MarketCap])
+        
+        for item in data:
+            print(item)
+        return data
+        
+    def write_to_csv(self, data):
+        f = open('ScrapedData.csv', 'w')
+        with f:
+            writer = csv.writer(f)
+            writer.writerow(['Date', 'Open', 'High', 'Low', 'Close', 'Volume', 'MarketCap'])
+        
+            for row in data:
+                writer.writerow(row)
