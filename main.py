70108b5319c3e03666c56bdd474f70fb9e96df39
kogito <kogito@lenovo>
2018-05-02 01:30:28 +0200

    update main to call correct files

---
+++ main.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Sep 23 19:05:35 2017
+@author: SuperKogito
+"""
+from gui import MainWindow
+
+
+def main():
+    MainWindow('Scraper')
+
+if __name__ == '__main__':
+    main()
